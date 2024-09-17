d345d0cc011e804b7d4be5ea16db3789fab3b7d2
deogodg <161716198+deogodg@users.noreply.github.com>
2024-09-17 20:37:11 -0300

    Add files via upload

---
+++ 2-trabalhandocomvariáveis.js
@@ -0,0 +1,20 @@
+//Console.log ("trabalhando com variáveis");
+console.log ("trabalhando com variáveis");
+//js é case sensitive
+
+ const idade = 29;
+const nome + "ricardo"
+let ano = 2024;
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 4-conversaodetipos.js
@@ -0,0 +1,12 @@
+console.log ("conversão de tipos");
+
+console.log ("ano" + 2024);
+console.log (parseInt("2") + parseInt("2"));
+
+
+
+console.log ("Ricardo" / "2"); //not a number
+
+
+console.log (6.5);
+console.log (6,5);
